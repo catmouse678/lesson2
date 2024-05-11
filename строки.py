41420ad473e22abf0198e4da62a52b0fcb48c412
catmouse678 <tiger.ks@mail.ru>
2024-05-11 17:17:07 +0300

    Add files via upload

---
+++ строки.py
@@ -0,0 +1,8 @@
+name=("это моя строка")
+print(name [0] )
+print(name[::-14])
+print(name[2:5])
+print(name[::-1])
+print(len(name))
+name=("это новая строка")
+print(("это новая строка") + (" это моя строка"))
